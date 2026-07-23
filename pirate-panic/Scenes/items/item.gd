0aaf0eea44f7baf5d0e652737f8e3841e26123fd
parmpreet001 <parmpreet1337@gmail.com>
2026-07-22 18:14:13 -0700

    Squashed commit of the following:
    commit 6b38029a6ea0d573b4e97f78e49c0626a4bf194a
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Wed Jul 22 18:10:30 2026 -0700
    
        Update interaction_test.tscn
    
    commit ecc254f200f0040a156002a305dd79647f9412c6
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Wed Jul 22 18:10:03 2026 -0700
    
        Update item.gd
    
    commit c3ae6e3289d06ee43d1d413860b8967a23f85219
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Wed Jul 22 18:09:07 2026 -0700
    
        Update Interactable.gd
    
    commit 5bf000a5d28f46cbd3435e8ec1aa88d57409a889
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Wed Jul 22 18:08:57 2026 -0700
    
        Update player.tscn
    
        - Add hand node as child of camera
    
    commit f37a05cc33c26f272f0896732d33cb828fd424dd
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Wed Jul 22 18:08:42 2026 -0700
    
        Update player.gd
    
        - Add hold_item()
        - Add drop_held_item()
    
    commit d27ef825f34c889a4ccf6f67da7192468cd6bcb0
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Wed Jul 22 18:08:20 2026 -0700
    
        Change project settings to add drop input

--- pirate-panic/Scenes/items/item.gd
+++ pirate-panic/Scenes/items/item.gd
@@ -1,10 +1,10 @@
-extends Node
+extends Node3D
 
 @export var item_name: String
+var starting_y: float # we need this to avoid clipping when items are dropped lol
 
-func on_interact():
-	print("player has interacted with item")
+func _ready():
+	starting_y = transform.origin.y		
 
-
-func _on_interactable_interacted() -> void:
-	on_interact()
+func _on_interactable_interacted(interactor: Node) -> void:
+	interactor.hold_item(self)
