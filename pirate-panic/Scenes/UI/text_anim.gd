5fd0818156909c3fbe899f3e1a69c05176d7255c
sweetautumndeer <amoulios@yahoo.com>
2026-07-23 19:13:09 -0700

    Optimized text scroll (shamefully)

--- pirate-panic/Scenes/UI/text_anim.gd
+++ pirate-panic/Scenes/UI/text_anim.gd
@@ -7,35 +7,46 @@ extends Node
 var type_delay : float # 1 / letters_per_second
 var full_text : String
 
+# reference to text label
+var label
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	start_text_scroll($RichTextLabel.text)
+	label = $RichTextLabel
+	start_text_scroll(label.text)
 
 func clear_text():
-	$RichTextLabel.text = ""
+	label.text = ""
 
 func start_text_scroll(text: String) -> void:
 	full_text = text
-	clear_text()
+	#clear_text()
 	type_delay = 1.0 / letters_per_second
+	label.visible_characters = 0
 	text_scroll()
 
 func text_scroll() -> void:
-	var bbcode_skip : bool
-	bbcode_skip = false
+	#var bbcode_skip : bool
+	#bbcode_skip = false
 	
-	for i in full_text.length():
-		$RichTextLabel.text += full_text[i]
-		if (full_text[i] == '[' or full_text[i] == '{' or full_text[i] == '/'): bbcode_skip = true
-		if (full_text.length() > i + 3 and full_text[i] == 'r' and full_text[i + 1] == 'e' and full_text[i + 2] == 's' and full_text[i + 3] == ':'): bbcode_skip = true
-		if (full_text[i] == ']' or full_text[i] == '}'): bbcode_skip = false
-		print($RichTextLabel.text)
-		
-		# don't wait for delay under these conditions
-		if (bbcode_skip): continue
-		#if (full_text[i] == ' '): continue
-		
+	
+	while label.visible_ratio < 1.0:
+		label.visible_characters += 1
 		await get_tree().create_timer(type_delay).timeout
+	
+	### GRAVEYARD OF SHAME FOR REIMPLEMENTING EXISTING FEATURE ###
+	#for i in full_text.length():
+	#	$RichTextLabel.text += full_text[i]
+	#	if (full_text[i] == '[' or full_text[i] == '{' or full_text[i] == '/'): bbcode_skip = true
+	#	if (full_text.length() > i + 3 and full_text[i] == 'r' and full_text[i + 1] == 'e' and full_text[i + 2] == 's' and full_text[i + 3] == ':'): bbcode_skip = true
+	#	if (full_text[i] == ']' or full_text[i] == '}'): bbcode_skip = false
+	#	print($RichTextLabel.text)
+	#	
+	#	# don't wait for delay under these conditions
+	#	if (bbcode_skip): continue
+	#	#if (full_text[i] == ' '): continue
+	#	
+	#	await get_tree().create_timer(type_delay).timeout
 	
 	await get_tree().create_timer(linger_time).timeout
 	clear_text()
