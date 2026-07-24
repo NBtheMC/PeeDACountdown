f756fe47d9c0b2e3da6d657939482bb8624f9485
parmpreet001 <parmpreet1337@gmail.com>
2026-07-24 11:23:19 -0700

    Squashed commit of the following:
    commit cb09ecb0f1b28851e4dd016fb160ea9c9bf6a12b
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Fri Jul 24 11:23:01 2026 -0700
    
        Replace leak damage/undamage meshes with new ones
    
    commit 7e91c661993a8950267f71916b2ae97e60e84fba
    Merge: 082d4b7 a81618c
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Fri Jul 24 11:18:47 2026 -0700
    
        Merge branch 'AD_Environment' into leak-system
    
    commit 082d4b78b6c08a6406b8d36d5b90238e8bb44f80
    Merge: 434c341 b526a4b
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Fri Jul 24 11:17:48 2026 -0700
    
        Merge branch 'main' into leak-system
    
    commit 434c341a0412d472ae5edf2a14b8ee014fe6be53
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Fri Jul 24 11:17:03 2026 -0700
    
        Update leak_system.gd
    
        - Add leak_max signal
        - Comment out some print messages
    
    commit a81618c04be40f144bd980b2bacd78e9b08e480f
    Author: Overcast <andrewdunne.gamedev@gmail.com>
    Date:   Fri Jul 24 11:04:52 2026 -0700
    
        Added damaged area models
    
    commit 1e991800c886ecc70342a309078f9d4b6e6118dd
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Fri Jul 24 10:57:24 2026 -0700
    
        Update leak_system.hd
    
    commit f2cccf3ad3a6063089351f50154bb8e0718cabb8
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Fri Jul 24 10:56:52 2026 -0700
    
        Comment out print statement in Interactable.gd
    
    commit 97398d5a6b940d2949d3ada8f10d22091105bf8b
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Fri Jul 24 10:56:44 2026 -0700
    
        Comment out print statements in playeer.gd
    
    commit ccce792fbc128b36f1f020b3490540192bd2d4d9
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Fri Jul 24 10:50:04 2026 -0700
    
        Update leak_system.tscn
    
    commit 99ae3325cf117095873eb9b5adcc61d05adf9170
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Fri Jul 24 10:49:54 2026 -0700
    
        Update leak_system.gd
    
        - Add export fields for min/max timer, and leak speed
        - Leak meter fills up based on num of leaks
    
    commit bf7bc1cea28654ab56daa5cf19cc65a71a3a7d3d
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Fri Jul 24 10:49:14 2026 -0700
    
        Update leak_spot.tscn
    
    commit 32726ae6e0e3c903f5ef4cf8dbcaf62a91d91e79
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Fri Jul 24 10:49:07 2026 -0700
    
        Update leak_spot.gd
    
        - Add field for text Node
        - Add funcs for Interactable signal emits
    
    commit 9d00cc448408a866e17c187699936c41bb7607c3
    Author: parmpreet001 <parmpreet1337@gmail.com>
    Date:   Fri Jul 24 10:48:40 2026 -0700
    
        Disable print statements in Interactable.gd

--- pirate-panic/Scenes/misc/Interactable.gd
+++ pirate-panic/Scenes/misc/Interactable.gd
@@ -7,17 +7,17 @@ signal showText(interactor: Node)
 signal unshowText(interactor: Node)
 
 func interact(interactor: Node) -> void:
-	print("interact signal called")
+	# print("interact signal called")
 	interacted.emit(interactor)
 
 func hold_interact(interactor: Node) -> void:
-	print("hold_interact signal called")
+	# print("hold_interact signal called")
 	hold_interacted.emit(interactor)
 
 func show_text(interactor: Node) -> void:
-	print("show_text signal called")
+	# print("show_text signal called")
 	showText.emit(interactor)
 
 func unshow_text(interactor: Node) -> void:
-	print("unshow_text signal called")
+	# print("unshow_text signal called")
 	unshowText.emit(interactor)
