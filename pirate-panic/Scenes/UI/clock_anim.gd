b3e5777394ab9c13387bafa4d1c41e3c32c58ce0
sweetautumndeer <amoulios@yahoo.com>
2026-07-23 17:41:52 -0700

    Clock hand actually ticks every second

--- pirate-panic/Scenes/UI/clock_anim.gd
+++ pirate-panic/Scenes/UI/clock_anim.gd
@@ -2,10 +2,17 @@ extends Node
 
 # progress of clock to completion (range: 0 - 1)
 var progress : float = 0
+# how often the clock hand ticks (in seconds)
+var tick_frequency : float = 1.0
+var tick_tween : Tween
 
 # rotations (in Degrees)
 @export var starting_rotation : float
 @export var final_rotation : float
+
+@export var time_until_completion : float = 10.0
+var time_elapsed : float = 0.0
+var last_tick : float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,6 +21,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$ClockHand.offset_transform_rotation = deg_to_rad(((1 - progress) * starting_rotation) + (progress * final_rotation))
-	progress += 0.1 * delta
+	progress += (1 / time_until_completion) * delta
+	time_elapsed += delta
 	if (progress >= 1.0): progress = 1.0
+	
+	if (time_elapsed > last_tick + tick_frequency):
+		last_tick = time_elapsed
+		animate_hand()
+
+func animate_hand() -> void:
+	tick_tween = get_tree().create_tween().bind_node(self)
+	var new_rotation = deg_to_rad(((1 - progress) * starting_rotation) + (progress * final_rotation))
+	tick_tween.tween_property($ClockHand, "offset_transform_rotation", new_rotation, 0.5).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
