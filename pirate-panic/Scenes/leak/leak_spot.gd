32726ae6e0e3c903f5ef4cf8dbcaf62a91d91e79
parmpreet001 <parmpreet1337@gmail.com>
2026-07-24 10:49:07 -0700

    Update leak_spot.gd
    - Add field for text Node
    - Add funcs for Interactable signal emits

--- pirate-panic/Scenes/leak/leak_spot.gd
+++ pirate-panic/Scenes/leak/leak_spot.gd
@@ -3,7 +3,17 @@ class_name LeakSpot
 
 var index: int
 
+@export var screen_text: Node
+
 signal leak_repair
 
 func _on_interactable_interacted(interactor: Node) -> void:
 	leak_repair.emit(index)
+
+
+func _on_interactable_show_text(interactor: Node) -> void:
+	screen_text.visible = true
+
+
+func _on_interactable_unshow_text(interactor: Node) -> void:
+	screen_text.visible = false
