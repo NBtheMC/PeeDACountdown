24a6f86a83b2c438e45a48b41441d07fb1fa1056
parmpreet001 <parmpreet1337@gmail.com>
2026-07-25 11:56:25 -0700

    Update item.gd
    - add starting_transform field
    - comment out print statements

--- pirate-panic/Scenes/items/item.gd
+++ pirate-panic/Scenes/items/item.gd
@@ -4,25 +4,26 @@ class_name Item
 @export var item_name: String
 @export var text : RichTextLabel
 
-var starting_y: float # we need this to avoid clipping when items are dropped lol
+var starting_transform: Transform3D # we need this to avoid clipping when items are dropped lol
 
 func _ready():
 	if text != null:
 		text.visible = false
-	starting_y = transform.origin.y
+	starting_transform = transform
+	print("starting transform of ", item_name, "is: ", starting_transform)
 
 func _on_interactable_interacted(interactor: Node) -> void:
-	print("Holding " + item_name)
+	# print("Holding " + item_name)
 	interactor.hold_item(self)
 
 func _on_interactable_show_text(interactor: Node) -> void:
-	print("Showing text of " + item_name)
+	# print("Showing text of " + item_name)
 	if text != null:
 		text.visible = true
 	pass # Replace with function body.
 
 func _on_interactable_unshow_text(interactor: Node) -> void:
-	print("Unshowing text of " + item_name)
+	# print("Unshowing text of " + item_name)
 	if text != null:
 		text.visible = false
 	pass # Replace with function body.
