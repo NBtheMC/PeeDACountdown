cbd1481fe4cd68dca3aabf70b6c787d4012b977e
sweetautumndeer <amoulios@yahoo.com>
2026-07-25 14:14:03 -0700

    Leak SFX Implemented

--- pirate-panic/Scenes/leak/leak_spot.gd
+++ pirate-panic/Scenes/leak/leak_spot.gd
@@ -4,12 +4,21 @@ class_name LeakSpot
 var index: int
 
 @export var screen_text: Node
+@export var sfx_leak_spew : AudioStreamPlayer3D
+@export var sfx_leak_fix : AudioStreamPlayer
 
 signal leak_repair
+
+func on_activate() -> void:
+	sfx_leak_spew.play()
+
+func on_deactivate() -> void:
+	sfx_leak_spew.stop()
 
 func _on_interactable_interacted(interactor: Node) -> void:
 	if (interactor.held_item != null && interactor.held_item.item_name == "mallet"):
 		leak_repair.emit(index)
+		sfx_leak_fix.play()
 
 
 func _on_interactable_show_text(interactor: Node) -> void:
